dac25f29d7c91bdea022fb84b2fad2198d98f6e5
adityanugroho18 <ds.adityanugroho@gmail.com>
2024-08-24 22:02:56 +0700

    Update settings.py

--- accounting_project/settings.py
+++ accounting_project/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         "NAME": 'db_akuntansi',
         "USER": 'root',
         "PASSWORD": '',
-        "HOST": 'localhost',
+        "HOST": '127.0.0.1',
         "PORT": 3306,
     }
 }
